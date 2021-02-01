aab2f619ea9678d7c6d12e9a846c5bdc6da6b688
OJWatson <oj.watson@hotmail.co.uk>
2021-02-01 13:33:13 +0000

    More Sensible Defaults for Control

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { reffRaw } from './reff.js';
 
 export const runModel = function(parameters, control = null) {
   if (control === null) {
-    control = {atol: 1e-3, rtol: 1e-3, stepSizeMin: 1e-5, stepSizeMax: 0.5, stepSizeMinAllow: true};
+    control = {atol: 1e-6, rtol: 1e-6, stepSizeMin: 1e-8, stepSizeMax: 0.5, stepSizeMinAllow: true};
   }
 
   const model = getModel();
