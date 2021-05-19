2a5dbb9264346f820478e50ed80cf11ff4e217fe
Giovanni Charles <giovanni.charles@gmail.com>
2021-05-19 18:28:01 +0100

    Implement new reff function

--- R/reff_run.R
+++ R/reff_run.R
@@ -39,7 +39,12 @@ write_json(
 )
 
 write_json(
-  output$odin_parameters[c('prob_hosp', 'vaccine_efficacy_infection')],
+  output$odin_parameters[c(
+    'prob_hosp',
+    'vaccine_efficacy_infection',
+    'tt_vaccine_efficacy_disease',
+    'tt_vaccine_efficacy_infection'
+  )],
   file.path(out_dir, paste0('pars_reff.json')),
   pretty = TRUE,
   matrix = 'columnmajor',
